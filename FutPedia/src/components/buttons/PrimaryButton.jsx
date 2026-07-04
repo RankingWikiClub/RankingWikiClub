c0efa4a11db5a47cd2e741556f5105a3355db900
futpediabd <futpediabd@gmail.com>
2026-07-04 17:59:32 -0300

    Delete FutPedia directory

--- FutPedia/src/components/buttons/PrimaryButton.jsx
+++
@@ -1,9 +0,0 @@
-function PrimaryButton({ children, type = "button", onClick }) {
-  return (
-    <button type={type} onClick={onClick} className="btn btn-primary">
-      {children}
-    </button>
-  );
-}
-
-export default PrimaryButton;
